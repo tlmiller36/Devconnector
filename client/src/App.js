6eff681cb1a311dd4000c61ca24df1ed9ae7a2bd
Tracy Miller <tlmiller36@gmail.com>
2019-10-11 12:06:30 -0700

    create login/register

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 
 function App() {
   return (
+  <Router>
     <div className="App">
-      <h1>My React App</h1>
+      <Navbar/>
+      <Route exact path="/" component={Landing} />
+      <div className="container">
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </div>
+      <Footer/>
     </div>
+  </Router>
   );
 }
 
